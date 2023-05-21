e1aaa1214592c4442dc640605b1ded94b668fb10
Tenesh Vignesan <teneshdev@gmail.com>
2023-05-22 02:22:39 +0800

    feat: Add initial modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PlansModule } from './modules/plans/plans.module';
+import { UsersModule } from './modules/users/users.module';
+import { ProfilesModule } from './modules/profiles/profiles.module';
+import { WalletsModule } from './modules/wallets/wallets.module';
+import { TransactionsModule } from './modules/transactions/transactions.module';
+import { CategoriesModule } from './modules/categories/categories.module';
+import { SubscriptionsModule } from './modules/subscriptions/subscriptions.module';
 
 @Module({
-  imports: [],
+  imports: [
+    PlansModule,
+    UsersModule,
+    ProfilesModule,
+    WalletsModule,
+    TransactionsModule,
+    CategoriesModule,
+    SubscriptionsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

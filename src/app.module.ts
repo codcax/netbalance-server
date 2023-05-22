e503ac190f57c802e81601123308a4f87f89524d
Tenesh Vignesan <teneshdev@gmail.com>
2023-05-22 09:17:34 +0800

    build: Add prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PlansModule } from './modules/plans/plans.module';
@@ -8,9 +9,15 @@ import { WalletsModule } from './modules/wallets/wallets.module';
 import { TransactionsModule } from './modules/transactions/transactions.module';
 import { CategoriesModule } from './modules/categories/categories.module';
 import { SubscriptionsModule } from './modules/subscriptions/subscriptions.module';
+import { PrismaModule } from './services/prisma/prisma.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      expandVariables: true,
+      envFilePath: `.env.${process.env.NODE_ENV}`,
+    }),
     PlansModule,
     UsersModule,
     ProfilesModule,
@@ -18,6 +25,7 @@ import { SubscriptionsModule } from './modules/subscriptions/subscriptions.modul
     TransactionsModule,
     CategoriesModule,
     SubscriptionsModule,
+    PrismaModule,
   ],
   controllers: [AppController],
   providers: [AppService],

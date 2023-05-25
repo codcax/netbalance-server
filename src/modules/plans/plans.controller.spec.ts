a08a5493664e253bfc01997983668ca047c8709a
Tenesh Vignesan <teneshdev@gmail.com>
2023-05-25 10:17:09 +0800

    test: Plan test case

--- src/modules/plans/plans.controller.spec.ts
+++ src/modules/plans/plans.controller.spec.ts
@@ -1,18 +1,58 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PlansController } from './plans.controller';
+import { PlansService } from './plans.service';
+import { FreePlan, BasicPlan, ProPlan } from '@data/plan';
+import { Plan } from './plan.entity';
 
 describe('PlansController', () => {
   let controller: PlansController;
+  let service: PlansService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [PlansController],
+      providers: [
+        {
+          provide: PlansService,
+          useValue: {
+            findAll: jest.fn(),
+            findOne: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     controller = module.get<PlansController>(PlansController);
+    service = module.get<PlansService>(PlansService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('findAll', () => {
+    it('should return an array of plans', async () => {
+      const mockPlans: Plan[] = [FreePlan, BasicPlan, ProPlan];
+
+      jest.spyOn(service, 'findAll').mockResolvedValue(mockPlans);
+
+      const result = await controller.findAll();
+
+      expect(result).toEqual(mockPlans);
+      expect(service.findAll).toHaveBeenCalled();
+    });
+  });
+
+  describe('findOne', () => {
+    it('should return a single plan', async () => {
+      const mockPlan: Plan = FreePlan;
+
+      jest.spyOn(service, 'findOne').mockResolvedValue(mockPlan);
+
+      const result = await controller.findOne(1);
+
+      expect(result).toEqual(mockPlan);
+      expect(service.findOne).toHaveBeenCalledWith(1);
+    });
   });
 });
--- src/modules/plans/plans.service.spec.ts
+++ src/modules/plans/plans.service.spec.ts
@@ -1,18 +1,36 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PlansService } from './plans.service';
+import { getRepositoryToken } from '@mikro-orm/nestjs';
+import { Plan } from './plan.entity';
+import { EntityRepository } from '@mikro-orm/core';
 
 describe('PlansService', () => {
   let service: PlansService;
+  let repository: EntityRepository<Plan>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [PlansService],
+      providers: [
+        PlansService,
+        {
+          provide: getRepositoryToken(Plan),
+          useValue: {
+            findAll: jest.fn(),
+            findOneOrFail: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<PlansService>(PlansService);
+    repository = module.get<EntityRepository<Plan>>(getRepositoryToken(Plan));
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('plan repository should be defined', () => {
+    expect(repository).toBeDefined();
   });
 });

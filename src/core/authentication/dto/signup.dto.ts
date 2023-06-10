18c295230f9571e64d85210ea301b32c20041e01
Tenesh Vignesan <teneshdev@gmail.com>
2023-06-11 03:16:27 +0800

    feat: Add authentication dtos

---
+++ src/core/authentication/dto/signup.dto.ts
@@ -0,0 +1,39 @@
+import {
+  IsAlpha,
+  IsAlphanumeric,
+  IsEmail,
+  IsNotEmpty,
+  MinLength,
+} from 'class-validator';
+import { BannedUsernames } from '@data/banned-words';
+import { DoesNotContainBannedWords } from '@shared/validators/banned-words.validator';
+
+export class SignInDto {
+  @IsNotEmpty()
+  @DoesNotContainBannedWords(BannedUsernames)
+  @IsAlphanumeric()
+  @MinLength(4)
+  readonly username: string;
+
+  @IsNotEmpty()
+  @DoesNotContainBannedWords(BannedUsernames)
+  @IsEmail()
+  readonly email: string;
+
+  @IsNotEmpty()
+  @IsAlphanumeric()
+  @MinLength(8)
+  readonly password: string;
+
+  @IsNotEmpty()
+  @IsAlpha()
+  @DoesNotContainBannedWords(BannedUsernames)
+  @MinLength(2)
+  readonly first_name: string;
+
+  @IsNotEmpty()
+  @IsAlpha()
+  @DoesNotContainBannedWords(BannedUsernames)
+  @MinLength(2)
+  readonly last_name: string;
+}

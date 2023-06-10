c60f0a43c5435c4c7b8ab37f79efcdbe6b17a358
Tenesh Vignesan <teneshdev@gmail.com>
2023-06-11 03:16:44 +0800

    feat: Add banned words constant

---
+++ src/data/banned-words.ts
@@ -0,0 +1,13 @@
+export const BannedUsernames: Array<string> = [
+  'admin',
+  'super',
+  'power',
+  'netbalance',
+  'Netbalance',
+  'NetBalance',
+  'public',
+  'dev',
+  'mail',
+  'hello',
+  'user',
+];

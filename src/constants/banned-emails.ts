4c1ec0376934a816d5b2d5dc575561d71d2c009d
Tenesh Vignesan <teneshdev@gmail.com>
2023-06-18 07:18:09 +0800

    feat: Add banned emails

---
+++ src/constants/banned-emails.ts
@@ -0,0 +1,12 @@
+export const BannedEmails: Array<string> = [
+  'admin',
+  'super',
+  'power',
+  'netbalance',
+  'Netbalance',
+  'NetBalance',
+  'public',
+  'dev',
+  'hello',
+  'user',
+];

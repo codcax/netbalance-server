37059fa2c287450cef77796681ff5b47b141389e
Tenesh Vignesan <teneshdev@gmail.com>
2023-06-18 07:20:04 +0800

    feat: Add create user method

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,20 +1,44 @@
 import { InjectRepository } from '@mikro-orm/nestjs';
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { User } from './user.entity';
-import { EntityRepository } from '@mikro-orm/postgresql';
+import { EntityManager, EntityRepository } from '@mikro-orm/postgresql';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
 export class UserService {
   constructor(
     @InjectRepository(User)
     private readonly userRepository: EntityRepository<User>,
+    private readonly entityManager: EntityManager,
   ) {}
 
-  async findOne(id: number): Promise<User> {
-    return this.userRepository.findOneOrFail({ id: id });
+  async findAll(): Promise<User[]> {
+    return this.userRepository.findAll();
   }
 
-  async createOne(user: User): Promise<User> {
-    return this.userRepository.create(user);
+  async findOne(id: number): Promise<User> {
+    return this.userRepository.findOne({ id: id });
+  }
+
+  async findByEmail(email: string): Promise<User> {
+    return this.userRepository.findOne({ email: email });
+  }
+
+  async createOne(data: CreateUserDto): Promise<User> {
+    const { username, email, password } = data;
+
+    const existingUser = await this.userRepository.findOne({ email: email });
+
+    if (existingUser) {
+      throw new BadRequestException({
+        message: 'Email is already taken.',
+      });
+    }
+
+    const user = new User(username, email, password);
+
+    await this.entityManager.persistAndFlush(user);
+
+    return user;
   }
 }

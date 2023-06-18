4fda239446a083a1c6cb3c49044e0da4e369c6f2
Tenesh Vignesan <teneshdev@gmail.com>
2023-06-18 20:43:02 +0800

    test: Add user controller test

--- src/modules/user/user.controller.spec.ts
+++ src/modules/user/user.controller.spec.ts
@@ -1,18 +1,51 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserController } from './user.controller';
+import { UserService } from './user.service';
+import { User } from './user.entity';
 
 describe('UserController', () => {
   let controller: UserController;
+  let service: UserService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UserController],
+      providers: [
+        {
+          provide: UserService,
+          useValue: {
+            findOne: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     controller = module.get<UserController>(UserController);
+    service = module.get<UserService>(UserService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+
+  describe('findOne', () => {
+    it('should return a single user', async () => {
+      const mockUser: User = new User(
+        'teneshvignesan',
+        'teneshbecs@gmail.com',
+        'abc1234567',
+      );
+
+      jest.spyOn(service, 'findOne').mockResolvedValue(mockUser);
+
+      const result = await controller.findOne(1);
+
+      expect(result).toEqual(mockUser);
+      expect(service.findOne).toHaveBeenCalledWith(1);
+    });
   });
 });

b3f91baeb7ee36cd5d82d0660b745f98ac2a0874
Tenesh Vignesan <teneshdev@gmail.com>
2023-06-18 21:21:59 +0800

    test: Update plan tests case

--- src/modules/plan/plan.controller.spec.ts
+++ src/modules/plan/plan.controller.spec.ts
@@ -2,11 +2,19 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { PlanController } from './plan.controller';
 import { PlanService } from './plan.service';
 import { FreePlan, BasicPlan, ProPlan } from '@constants/plan';
-import { Plan } from './plan.entity';
 
 describe('PlanController', () => {
   let controller: PlanController;
-  let service: PlanService;
+
+  const mockPlanService = {
+    findAll: jest.fn().mockImplementation(() => {
+      return Promise.resolve([FreePlan, BasicPlan, ProPlan]);
+    }),
+
+    findOne: jest.fn().mockImplementation(() => {
+      return Promise.resolve(FreePlan);
+    }),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -14,49 +22,29 @@ describe('PlanController', () => {
       providers: [
         {
           provide: PlanService,
-          useValue: {
-            findAll: jest.fn(),
-            findOne: jest.fn(),
-          },
+          useValue: mockPlanService,
         },
       ],
     }).compile();
 
     controller = module.get<PlanController>(PlanController);
-    service = module.get<PlanService>(PlanService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
-
   describe('findAll', () => {
     it('should return an array of plans', async () => {
-      const mockPlans: Plan[] = [FreePlan, BasicPlan, ProPlan];
-
-      jest.spyOn(service, 'findAll').mockResolvedValue(mockPlans);
-
       const result = await controller.findAll();
-
-      expect(result).toEqual(mockPlans);
-      expect(service.findAll).toHaveBeenCalled();
+      expect(result).toStrictEqual([FreePlan, BasicPlan, ProPlan]);
     });
   });
 
   describe('findOne', () => {
     it('should return a single plan', async () => {
-      const mockPlan: Plan = FreePlan;
-
-      jest.spyOn(service, 'findOne').mockResolvedValue(mockPlan);
-
       const result = await controller.findOne(1);
-
-      expect(result).toEqual(mockPlan);
-      expect(service.findOne).toHaveBeenCalledWith(1);
+      expect(result).toEqual(FreePlan);
     });
   });
 });
--- src/modules/plan/plan.service.spec.ts
+++ src/modules/plan/plan.service.spec.ts
@@ -1,13 +1,21 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PlanService } from './plan.service';
+import { FreePlan, BasicPlan, ProPlan } from '@constants/plan';
 import { getRepositoryToken } from '@mikro-orm/nestjs';
 import { Plan } from './plan.entity';
-import { EntityRepository } from '@mikro-orm/postgresql';
-import { FreePlan, BasicPlan, ProPlan } from '@constants/plan';
 
 describe('PlanService', () => {
   let service: PlanService;
-  let repository: EntityRepository<Plan>;
+
+  const mockPlanRepository = {
+    findAll: jest.fn().mockImplementation(() => {
+      return Promise.resolve([FreePlan, BasicPlan, ProPlan]);
+    }),
+
+    findOne: jest.fn().mockImplementation(() => {
+      return Promise.resolve(FreePlan);
+    }),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -15,49 +23,29 @@ describe('PlanService', () => {
         PlanService,
         {
           provide: getRepositoryToken(Plan),
-          useValue: {
-            findAll: jest.fn(),
-            findOneOrFail: jest.fn(),
-          },
+          useValue: mockPlanRepository,
         },
       ],
     }).compile();
 
     service = module.get<PlanService>(PlanService);
-    repository = module.get<EntityRepository<Plan>>(getRepositoryToken(Plan));
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
 
-  it('plan repository should be defined', () => {
-    expect(repository).toBeDefined();
-  });
-
   describe('findAll', () => {
     it('should return an array of plans', async () => {
-      const mockPlans: Plan[] = [FreePlan, BasicPlan, ProPlan];
-
-      jest.spyOn(service, 'findAll').mockResolvedValue(mockPlans);
-
       const result = await service.findAll();
-
-      expect(result).toEqual(mockPlans);
-      expect(service.findAll).toHaveBeenCalled();
+      expect(result).toStrictEqual([FreePlan, BasicPlan, ProPlan]);
     });
   });
 
   describe('findOne', () => {
-    it('should return a single plan', async () => {
-      const mockPlan: Plan = FreePlan;
-
-      jest.spyOn(service, 'findOne').mockResolvedValue(mockPlan);
-
+    it('should return a plan', async () => {
       const result = await service.findOne(1);
-
-      expect(result).toEqual(mockPlan);
-      expect(service.findOne).toHaveBeenCalledWith(1);
+      expect(result).toEqual(FreePlan);
     });
   });
 });

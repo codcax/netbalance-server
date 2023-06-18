2d3a8d6f809ebb55f737713177c4fc622be5604b
Tenesh Vignesan <teneshdev@gmail.com>
2023-06-18 20:42:19 +0800

    chore: Refactor lint

--- src/modules/plan/plan.service.ts
+++ src/modules/plan/plan.service.ts
@@ -9,9 +9,11 @@ export class PlanService {
     @InjectRepository(Plan)
     private readonly planRepository: EntityRepository<Plan>,
   ) {}
+
   async findAll(): Promise<Plan[]> {
     return this.planRepository.findAll();
   }
+
   async findOne(id: number): Promise<Plan> {
     return this.planRepository.findOne({ id: id });
   }
